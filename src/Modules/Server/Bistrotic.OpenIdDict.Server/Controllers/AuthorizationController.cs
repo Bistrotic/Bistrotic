558f94aa2f6072381959b0b281941d004e029f6d
Jérôme Piquot <jpiquot@fiveforty.fr>
2021-02-25 21:11:56 +0100

    fix

--- src/Modules/Server/Bistrotic.OpenIdDict.Server/Controllers/AuthorizationController.cs
+++ src/Modules/Server/Bistrotic.OpenIdDict.Server/Controllers/AuthorizationController.cs
@@ -372,7 +372,7 @@ namespace Bistrotic.OpenIdDict.Controllers
             throw new InvalidOperationException("The specified grant type is not supported.");
         }
 
-        private IEnumerable<string> GetDestinations(Claim claim, ClaimsPrincipal principal)
+        private static IEnumerable<string> GetDestinations(Claim claim, ClaimsPrincipal principal)
         {
             // Note: by default, claims are NOT automatically included in the access and identity tokens.
             // To allow OpenIddict to serialize them, you must attach them a destination, that specifies

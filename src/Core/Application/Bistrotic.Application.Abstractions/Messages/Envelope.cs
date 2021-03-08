99161db37e91f646d8c22e5d79aec61d5de7a56f
Jérôme Piquot <jpiquot@fiveforty.fr>
2021-03-08 09:57:59 +0100

    add causation id

--- src/Core/Application/Bistrotic.Application.Abstractions/Messages/Envelope.cs
+++ src/Core/Application/Bistrotic.Application.Abstractions/Messages/Envelope.cs
@@ -6,11 +6,12 @@ namespace Bistrotic.Application.Messages
     public class Envelope<T> : IEnvelope
         where T : IMessage
     {
-        public Envelope(T message, UserName userName, MessageId? correlationId = null)
+        public Envelope(T message, UserName userName, MessageId? correlationId = null, MessageId? causationId = null)
         {
             UserName = userName;
             Message = message;
             CorrelationId = correlationId;
+            CausationId = causationId;
         }
 
         public Envelope(T message, IEnvelope parent)
@@ -18,6 +19,7 @@ namespace Bistrotic.Application.Messages
             UserName = parent.UserName;
             Message = message;
             CorrelationId = parent.CorrelationId ?? parent.Message.MessageId;
+            CausationId = parent.Message.MessageId;
         }
 
         public Envelope(IEnvelope envelope)
@@ -25,9 +27,11 @@ namespace Bistrotic.Application.Messages
             UserName = envelope.UserName;
             Message = (T)envelope.Message;
             CorrelationId = envelope.CorrelationId;
+            CausationId = envelope.CausationId;
         }
 
         public MessageId? CorrelationId { get; }
+        public MessageId? CausationId { get; }
         public T Message { get; }
         public UserName UserName { get; }
         IMessage IEnvelope.Message => Message;

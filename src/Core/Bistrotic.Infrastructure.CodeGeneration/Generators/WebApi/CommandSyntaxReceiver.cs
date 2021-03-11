ef2a7a580a87fb6fca6398e1842a210c64a15ec8
Jérôme Piquot <jpiquot@fiveforty.fr>
2021-03-11 22:47:30 +0100

    add syntax node search

--- src/Core/Bistrotic.Infrastructure.CodeGeneration/Generators/WebApi/CommandSyntaxReceiver.cs
+++ src/Core/Bistrotic.Infrastructure.CodeGeneration/Generators/WebApi/CommandSyntaxReceiver.cs
@@ -3,17 +3,55 @@
     using System.Collections.Generic;
 
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
 
     internal class CommandSyntaxReceiver : ISyntaxReceiver
     {
-        public List<SyntaxNode> CandidateProxies { get; } = new List<SyntaxNode>();
+        public List<ClassDeclarationSyntax> CandidateProxies { get; } = new List<ClassDeclarationSyntax>();
 
         public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
         {
-            if (syntaxNode is SyntaxNode classSyntax && classSyntax.ContainsDirectives)
+            if (syntaxNode is ClassDeclarationSyntax classSyntax && !IsAbstract(classSyntax) && HasInterface("Bistrotic.Application.Commands.ICommand", classSyntax))
             {
                 CandidateProxies.Add(classSyntax);
             }
+        }
+
+        private static bool Implements(INamedTypeSymbol symbol, ITypeSymbol type)
+        {
+            foreach (var s in symbol.AllInterfaces)
+            {
+                if (SymbolEqualityComparer.Default.Equals(s, type))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool IsAbstract(ClassDeclarationSyntax node)
+        {
+            foreach (var modifier in node.Modifiers)
+            {
+                if (modifier.IsKind(SyntaxKind.AbstractKeyword))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool HasInterface(string commandInterfaceFullName, ClassDeclarationSyntax classSyntax)
+        {
+            var types = classSyntax.BaseList?.Types;
+            if (types != null)
+            {
+                foreach (var t in types)
+                {
+                }
+            }
+            return false;
         }
     }
 }

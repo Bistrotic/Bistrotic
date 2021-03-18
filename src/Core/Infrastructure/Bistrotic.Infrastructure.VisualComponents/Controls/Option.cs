5e9ae47e6a10dc7a181496a55fa18c053e08c552
Jérôme Piquot <jpiquot@fiveforty.fr>
2021-03-18 15:13:54 +0100

    fix razor issue

--- src/Core/Infrastructure/Bistrotic.Infrastructure.VisualComponents/Controls/Option.cs
+++ src/Core/Infrastructure/Bistrotic.Infrastructure.VisualComponents/Controls/Option.cs
@@ -1,6 +1,8 @@
 ﻿namespace Bistrotic.Infrastructure.VisualComponents.Controls
 {
+#pragma warning disable CA1716 // Identifiers should not match keywords
     public class Option : BlazorComponent
+#pragma warning restore CA1716 // Identifiers should not match keywords
     {
     }
 }

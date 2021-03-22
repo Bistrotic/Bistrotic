d51caa92b98d4877bb9f8e37d4d23605491f8586
Jérôme Piquot <jpiquot@fiveforty.fr>
2021-03-22 21:34:23 +0100

    add read email

--- src/Core/Infrastructure/Bistrotic.Infrastructure.MicrosoftGraph/GraphService.cs
+++ src/Core/Infrastructure/Bistrotic.Infrastructure.MicrosoftGraph/GraphService.cs
@@ -1,8 +1,11 @@
 ﻿namespace Bistrotic.Infrastructure.MicrosoftGraph
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+
+    using Bistrotic.Domain.Communication;
 
     using Microsoft.Graph;
 
@@ -37,7 +40,62 @@
             return ids;
         }
 
+        public async Task<IEnumerable<Email>> GetUserMails(string userPrincipalName)
+        {
+            var messages = await GraphClient
+                .Users[userPrincipalName]
+                .Messages
+                .Request()
+                .Expand(p => p.Attachments)
+                .GetAsync()
+                .ConfigureAwait(false);
+
+            List<Email> ids = new(messages.Select(p => Map(p)));
+            while (messages.NextPageRequest != null)
+            {
+                messages = await messages
+                    .NextPageRequest
+                    .GetAsync()
+                    .ConfigureAwait(false);
+                ids.AddRange(messages.Select(p => Map(p)));
+            }
+            return ids;
+        }
+
         private GraphServiceClient InitializeGraphClient()
               => new(AuthenticationService.AuthenticationProvider);
+
+        private Email Map(Message message)
+        {
+            return new Email
+            {
+                Subject = message.Subject ?? string.Empty,
+                Body = message.Body.Content ?? string.Empty,
+                Sender = message.Sender.EmailAddress.Address ?? string.Empty,
+                ToRecipients = message.ToRecipients?.Select(p => p.EmailAddress.Address)?.ToArray() ?? Array.Empty<string>(),
+                CopyToRecipients = message.CcRecipients?.Select(p => p.EmailAddress.Address)?.ToArray() ?? Array.Empty<string>(),
+                Attachments = Map(message.Attachments)
+            };
+        }
+
+        private IReadOnlyList<EmailAttachment> Map(IMessageAttachmentsCollectionPage? attachments)
+        {
+            if (attachments == null || attachments.Count == 0)
+            {
+                return Array.Empty<EmailAttachment>();
+            }
+            List<EmailAttachment> files = new List<EmailAttachment>(attachments.Select(p => Map(p)));
+            while (attachments.NextPageRequest != null)
+            {
+                attachments = attachments.NextPageRequest.GetAsync().GetAwaiter().GetResult();
+                files.AddRange(attachments.Select(p => Map(p)));
+            }
+            return files;
+        }
+
+        private EmailAttachment Map(Attachment attachment)
+        {
+            return new EmailAttachment { Name = attachment.Name };
+        }
     }
 }
